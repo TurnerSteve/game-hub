b633dd276b0fc8567d0e5e8516c4ca161b412c5d
StephenDTurner <frawdo@icloud.com>
2024-07-13 10:59:34 +0100

    Added RAWG API key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "20bd0c11a3794251bd390bb18ff9664e",
   },
 });
